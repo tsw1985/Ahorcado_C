8cebd926ab8e297745348514f4f4f46005732eae
G85 <gabrielgg85@gmail.com>
2024-04-21 19:53:53 +0100

    G85
    Missing check if user repeat letter

--- src/main.cpp
+++ src/main.cpp
@@ -9,37 +9,41 @@
 int main(int argc, char const *argv[])
 {
 
+    system("clear");
     printf("Bienvenido al juego del ahorcado. Tienes 5 oportunidades. Introduce la palabra\n");
-    char wordToPlay[15];
-    char wordToShow[15];
+    char wordToPlay[20];
+    char wordToShow[20];
 
-    fgets(wordToPlay,sizeof(wordToPlay),stdin);
+    scanf("%s",wordToPlay);
     wordToPlay[strcspn(wordToPlay, "\n")] = '\0'; // Elimina el carácter de nueva línea
-    //printf("A jugar %s\n",wordToPlay);
+
     memset(wordToShow,'*',strlen(wordToPlay)); // rellenamos de *
+    system("clear");
     printf("%s\n",wordToShow);
+    printf("Tienes que averiguar %d letras\n",(int)strlen(wordToShow));
 
 
     int lifes = 5;
+    int win = 0;
     int *matchsList = NULL;
     while(lifes > 0){
 
         char letter[2];
-        printf("?:->\n");
-        fgets(letter,sizeof(letter),stdin);
-        letter[strcspn(letter, "\n")] = '\0'; // Elimina el carácter de nueva línea
-        letter[strcspn(letter, "\n")] = '\0'; // Elimina el carácter de nueva línea
+        printf("?:-> ");
+        scanf("%s",letter);
+        letter[strcspn(letter, "\n")] = '\0'; // \0 Elimina el carácter de nueva línea
 
         //list of matchs
         matchsList = indexOf(wordToPlay,letter);
         if(matchsList != NULL){
 
             int exists_match = 0;
-            for(int index_match_list = 0 ; index_match_list < (int)sizeof(matchsList) / (int)sizeof(matchsList[0]) ; index_match_list++){
+            for(int index_match_list = 0 ; index_match_list <= (int)sizeof(matchsList) / (int)sizeof(matchsList[0]) ; index_match_list++){
                     int match = matchsList[index_match_list];
                     if(match != -1){
                         exists_match = 1;
                         wordToShow[match] = letter[0];
+                        win++;
                     }
             }
 
@@ -48,10 +52,15 @@ int main(int argc, char const *argv[])
                 printf("PERDISTE, te quedan %d oportunidades\n",lifes);
             }
 
+            if(win == (int)strlen(wordToShow)){
+                printf("BIEN !! HAS GANADO!!\n");
+                break;
+            }
+
 
         }
 
-        printf("Sigue jugando ...\n");
+        printf("Sigues jugando ...\n");
         printf("%s\n",wordToShow);
         
  
